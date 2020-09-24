9980ddebff9794ef7e51ff4393ae7374208e1253
N'don Daniel Hermann <49002247+NdonDaniel@users.noreply.github.com>
2020-09-24 12:12:12 +0000

    Update sreial-test.py

--- kamailio/Others/sreial-test.py
+++ kamailio/Others/sreial-test.py
@@ -2,6 +2,7 @@
 
 
 
+# @@@@@@@@@@@@@@@@@@@@@@ ÇA PASSE PAS @@@@@@@@@@@@@@@@@@
 # Winner functions
 route[WINS]{
 	sql_query("locA", "select * from location where username=$rU", "res_loc");
@@ -42,3 +43,52 @@ failure_route["serial"]
   t_on_failure("serial");
   t_relay();
 }
+
+---------------------------------------------------------------------------------
+
+# @@@@@@@@@@@@ ÇA PASSE PAS @@@@@@@@@@@@@@@@@@@@@#
+
+route[WINS_TEST]{
+
+	### Patch ### Read q-value from User-Agent header and use it.
+	if(is_method("REGISTER") && is_present_hf("Contact")){
+
+		$var(qval)=$(hdr(User-Agent){param.value,q});
+		
+		if(($var(qval) != $null) && ($var(qval) != "0") | $var(qval)!="-1.O"){
+			#xlog("L_NOTICE", "q-value set by the client is q=$var(qval) - R=$ru ID=$cin");
+			xlog("je ne suis pas null");
+			$var(newct) = $ct + ';q=' + "$var(qval)";
+			remove_hf("Contact");
+			#append_hf("Contact: $(var(newct))rn");
+		}else{
+			xlog("je suis null");
+			sql_query("db_connection","select * from location where username=$fU","res_Users");
+			$var(usernumb) = $dbr(res_Users=>rows);
+
+			if($var(usernumb)==1 | $var(usernumb)>1){
+				# un user trouvé ou plusieurs
+				$var(usernumb)= $var(usernumb)+3;
+				$var(newct) = $ct + ';q=0.' + '$var(usernumb)';
+				remove_hf("Contact");
+				#append_hf("Contact: $(var(newct))rn");
+				xlog("many Users or One");
+			}else{
+				# pas de location 
+				xlog("no User");
+				$var(newct) = $ct + ';q=0.' + 9;
+				remove_hf("Contact");
+				#append_hf("Contact: $(var(newct))rn");
+			}
+
+			xlog("$var(qval)");
+
+		}
+	}
+}
+
+---------------------------------------------------------------------------------------
+
+
+
+
