219885ba924c54dcba03c075efd50754e08692f9
N'don Daniel Hermann <49002247+NdonDaniel@users.noreply.github.com>
2021-01-15 14:03:10 +0000

    Create local-config.py

---
+++ 3cx/local-config.py
@@ -0,0 +1,19 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@ LOCAL 1 @@@@@@@@@@@@@@@@@@@@@@@
+-------- VM ---------
+V-Machine: 3CX-SERVER
+passwd: localserver
+
+----------------------- 3CX -----------
+
+Vous pouvez accéder à la console d'administration de 3CX ici : https://call.3cx.co.za:5001 ou via IP : https://154.68.5.44:5001
+
+Nom d'utilisateur : admin
+
+Mot de passe : admin@3CX0354
+
+IP publique : 154.68.5.44
+
+FQDN : call.3cx.co.za
+
+La clé de licence est : PDBM-LPEZ-IEYA-AXM7
+
