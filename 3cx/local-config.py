454de19da56827af6e9d34b2ef27028d06271c2b
N'don Daniel Hermann <49002247+NdonDaniel@users.noreply.github.com>
2021-01-20 12:38:55 +0000

    Update local-config.py

--- 3cx/local-config.py
+++ 3cx/local-config.py
@@ -5,18 +5,18 @@ passwd: localserver
 
 ----------------------- 3CX -----------
 
-Vous pouvez accéder à la console d'administration de 3CX ici : https://call.3cx.co.za:5001 ou via IP : https://154.68.5.44:5001
+
+Vous pouvez accéder à la console d'administration de 3CX ici : https://dwentreprise.3cx.co.za:5001 ou via IP : https://51.210.54.81:5001
 
 Nom d'utilisateur : admin
 
-Mot de passe : admin@3CX0354
+Mot de passe : Nan2021#23
 
-IP publique : 154.68.5.44
+IP publique : 51.210.54.81
 
-FQDN : call.3cx.co.za
+FQDN : dwentreprise.3cx.co.za
 
-La clé de licence est : PDBM-LPEZ-IEYA-AXM7
-
+La clé de licence est : PPFD-OXHY-YKVA-ANJB
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@ LOCAL 2 @@@@@@@@@@@@@@@@@@@@@@@
 
