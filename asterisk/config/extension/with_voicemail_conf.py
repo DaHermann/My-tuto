a0861ea42df1123a455c75bcad6237a28ff75d6d
N'don Daniel Hermann <49002247+DaHermann@users.noreply.github.com>
2021-08-27 16:54:05 +0000

    Update with_voicemail_conf.py

--- asterisk/config/extension/with_voicemail_conf.py
+++ asterisk/config/extension/with_voicemail_conf.py
@@ -1,25 +1,79 @@
+;=========================================== Projet Asterisk 1 ===================================
 
-;=========================================== Projet Asterisk 1 VOICEMAIL ===================================
 
-;=================== Boite vocal de l'administration ==============
-[adminMail] 
-1001 => 000,Sip phone 1
-1002 => 000,Sip phone 2
-1003 => 000,Sip phone 3
-1004 => 000,Sip phone 4
+[ADMIN] ;Context ADMIN
+exten => _1XXX,1,NoOp(attention il y a un appel en cours )
+same => n,Set(CHANNEL(language)=fr)
+same => n,Dial(PJSIP/${EXTEN},20) ; Appeler
+same => n,Voicemail(${EXTEN}@adminMail)
+same => n,Hangup() ; Racrocher
 
-;=================== Boite vocal du dévéloppement ==============
-[devMail]
-301 => 000,Sip phone 1
-302 => 000,Sip phone 2
-303 => 000,Sip phone 3
-304 => 000,Sip phone 4
-305 => 000,Sip phone 5
-306 => 000,Sip phone 6
+include => t-admin ; trunk sip admin
 
-;=================== Boite vocal de la securité reseau ==============
-[secuMail]
-7001 => 000,Sip phone 1
-7002 => 000,Sip phone 2
-7003 => 000,Sip phone 3
+;Boite vocal admin
+exten => _1000,1,NoOp(Je suis dans la boite vocal)
+same => n,Set(CHANNEL(language)=fr)
+same => n, VoiceMailMain(${CALLERID(num)}@adminMail,s)
+same => n, Hangup()
+
+
+[DEV] ;Context DEV
+exten => _3XX,1,NoOp(attention il y a un appel en cours )
+same => n,Set(CHANNEL(language)=fr)
+same => n,Dial(PJSIP/${EXTEN},20) ; Appeler
+same => n,Voicemail(${EXTEN}@devMail)
+same => n,Hangup() ; Racrocher
+
+include => t-dev ; trunk sip dev
+
+;Boite vocal dev
+exten => _300,1,NoOp(Je suis dans la boite vocal)
+same => n,Set(CHANNEL(language)=fr)
+same => n, VoiceMailMain(${CALLERID(num)}@devMail,s)
+same => n, Hangup()
+
+
+[SECU] ;Context SECU
+exten => _7XXX,1,NoOp(attention il y a un appel en cours )
+same => n,Set(CHANNEL(language)=fr)
+same => n,Dial(PJSIP/${EXTEN},20) ; Appeler
+same => n,Voicemail(${EXTEN})
+same => n,Hangup() ; Racrocher
+
+include => t-secu ; trunk sip secu
+
+exten => _7000,1,NoOp(Je suis dans la boite vocal)
+same => n,Set(CHANNEL(language)=fr)
+same => n, VoiceMailMain(${CALLERID(num)}@secuMail,s)
+same => n, Hangup()
+
+
+;========================= Trunck SIP ==============================
+
+[t-admin]
+exten => _3XX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(DEV,${EXTEN},1)
+same => n,Hangup()
+
+exten => _7XXX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(SECU,${EXTEN},1)
+same => n,Hangup()
+
+[t-dev]
+exten => _1XXX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(ADMIN,${EXTEN},1)
+same => n,Hangup()
+
+exten => _7XXX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(SECU,${EXTEN},1)
+same => n,Hangup()
+
+[t-secu]
+exten => _1XXX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(ADMIN,${EXTEN},1)
+same => n,Hangup()
+
+exten => _3XX,1,NoOp(Je suis dans la boite vocal)
+same => n,Goto(DEV,${EXTEN},1)
+same => n,Hangup()
 
